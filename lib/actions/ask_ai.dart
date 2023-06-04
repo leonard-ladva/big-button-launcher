cc0ad4a75f5bfd19de3cacea7b61c2a506ae3b81
Leonard Ladva <leonard.ladva@gmail.com>
2023-06-04 19:14:00 +0300

    WIP: beeping on load.

--- lib/actions/ask_ai.dart
+++ lib/actions/ask_ai.dart
@@ -2,17 +2,31 @@ import 'dart:developer';
 
 import 'package:bbl/speak.dart';
 import 'package:dart_openai/dart_openai.dart';
+import 'package:flutter_beep/flutter_beep.dart';
 
 void askAi(String prompt) async {
-  OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
-    model: "gpt-3.5-turbo",
-    messages: [
-      OpenAIChatCompletionChoiceMessageModel(
-        content: prompt,
-        role: OpenAIChatMessageRole.user,
-      ),
-    ],
-  );
-  log(chatCompletion.choices[0].message.content);
-  speak(chatCompletion.choices[0].message.content);
+  // start beeping
+  beep();
+  try {
+    OpenAIChatCompletionModel chatCompletion =
+        await OpenAI.instance.chat.create(
+      model: "gpt-3.5-turbo",
+      messages: [
+        OpenAIChatCompletionChoiceMessageModel(
+          content: prompt,
+          role: OpenAIChatMessageRole.user,
+        ),
+      ],
+    );
+    log(chatCompletion.choices[0].message.content);
+    speak(chatCompletion.choices[0].message.content);
+  } catch (e) {
+    log(e.toString());
+    speak(e.toString());
+  }
+  // stop beeping
+}
+
+void beep() {
+  FlutterBeep.beep();
 }
